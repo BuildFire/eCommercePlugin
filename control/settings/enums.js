c7cb1dbaff7018c0d211739e43d2fa284aa8d820
Ahmad Hamed <ahmad.fhamed@gmail.com>
2017-05-31 18:21:56 +0300

    Now currency section is dynamic; you can choose the predefined currencies or you can add you a new one

--- control/settings/enums.js
+++ control/settings/enums.js
@@ -3,6 +3,7 @@
 (function (angular) {
     angular.module('eCommercePluginSettings')
         .constant('TAG_NAMES', {
-            SHOPIFY_INFO: 'shopifyInfo'
+            SHOPIFY_INFO: 'shopifyInfo',
+            NEW_CURRENCY: 'newCurrency'
         });
 })(window.angular);
